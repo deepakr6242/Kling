b49fb544ddc864c8e7c9c3bcbba73920bbd58d6e
Deepak <deepakr6242@gmail.com>
2021-12-02 10:03:40 +0000

    Revert "'changed_default_batteries_authentication_and_logout'"
    This reverts commit 20bd26daf4bda0b9b2fa9dfda5186e3fd013db4c.

--- kling_app/urls.py
+++ kling_app/urls.py
@@ -1,18 +1,11 @@
-from django.urls import path,include
-from django.views.generic.base import TemplateView
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    # path('', views.home_page, name='home_page'),
-    # path(r'home/',views.home, name='home'),
-    # path(r'register/', views.register, name="register"),
-
-    # path(r"add_user/", views.add_user, name="add_user"), 
-
-    # path(r"add_user/", views.add_user, name="add_user"),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.home_page, name='home_page'),
+    path(r'home/',views.home, name='home'),
+    path(r'register/', views.register, name="register"),
+    path(r"add_user/", views.add_user, name="add_user"),
     
-
 ]
--- kling_app/views.py
+++ kling_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.template import loader
 import logging
 from django.views.decorators.cache import cache_control
-
+######################################
 from .forms import NewUserForm
 from django.contrib import messages
 from django.http import HttpResponse, response
@@ -11,7 +11,6 @@ from django.contrib.auth.hashers import make_password,check_password,is_password
 
 logging.basicConfig(filename='mylog.log', level=logging.DEBUG)
 logging.debug('start')
-
 
 
 
