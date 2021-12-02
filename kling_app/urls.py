20bd26daf4bda0b9b2fa9dfda5186e3fd013db4c
Deepak <deepakr6242@gmail.com>
2021-12-02 10:00:07 +0000

    'changed_default_batteries_authentication_and_logout'

--- kling_app/urls.py
+++ kling_app/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.urls import path,include
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
-    path(r'home/',views.home, name='home'),
-    path(r'register/', views.register, name="register"),
-    path(r"add_user/", views.add_user, name="add_user"),
+    # path('', views.home_page, name='home_page'),
+    # path(r'home/',views.home, name='home'),
+    # path(r'register/', views.register, name="register"),
+
+    # path(r"add_user/", views.add_user, name="add_user"), 
+
+    # path(r"add_user/", views.add_user, name="add_user"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     
+
 ]
